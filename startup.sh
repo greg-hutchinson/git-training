46222a31f80ad3af3db0e14b9127441138f9a873
Unknown <greg.hutchinson@fcc-fac.ca>
2020-06-08 15:59:44 -0600

    Changed startup directory

--- startup.sh
+++ startup.sh
@@ -1,2 +1,1 @@
 docker run -it -v /Users/hutching/git-repos/git-training:/repo -p 9000:9000 gitpitch/desktop:pro;
-#docker run -it -v d:/gitRepos/Personal/git-training:/repo -p 9000:9000 gitpitch/desktop:pro
