3635b23bc6fcd4c3a5ee3046e524bee3837c4a3f
Hutch <greg.hutchinson@fcc-fac.ca>
2021-08-27 10:45:08 -0600

    More cleanup

--- README.md
+++
@@ -1,2 +0,0 @@
-# git-training
-GitPitch presentation and notes on git
--- startup.bat
+++
@@ -1,2 +0,0 @@
-#docker run -it -v /Users/hutching/git-repos/git-training:/repo -p 9000:9000 gitpitch/desktop:pro;
-docker run -it -v d:/gitRepos/git-training:/repo -p 9000:9000 gitpitch/desktop:pro
